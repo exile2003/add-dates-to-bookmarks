80ffb13bb40b649f28b0babd1ffe87107b2cf93b
exile2003 <31631692+exile2003@users.noreply.github.com>
2023-07-13 18:24:03 +0200

    Trial using pull request - 6

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import FileSaver from 'file-saver';
 //import '../node_modules/jquery/dist/jquery.min.js';
 //import './js/bootstrap-filestyle.min.js';
 
+
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 /*
