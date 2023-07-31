1b8795e7321e744d13200c107d2b313ce71ec589
exile2003 <31631692+exile2003@users.noreply.github.com>
2023-07-31 23:04:20 +0200

    Got the app in order

--- webpack.config.js
+++ webpack.config.js
@@ -35,16 +35,16 @@ module.exports = {
             filename: path.resolve(__dirname, "dist", "index.html"),
             chunks: ['index']
         }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src/routes", "en.html"),
-            filename: path.resolve(__dirname, "dist/routes", "en.html"),
-            chunks: ['routes']
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src/routes", "ru.html"),
-            filename: path.resolve(__dirname, "dist/routes", "ru.html"),
-            chunks: ['routes']
-        }),
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, "src/routes", "en.html"),
+        //     filename: path.resolve(__dirname, "dist/routes", "en.html"),
+        //     chunks: ['routes']
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, "src/routes", "ru.html"),
+        //     filename: path.resolve(__dirname, "dist/routes", "ru.html"),
+        //     chunks: ['routes']
+        // }),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
